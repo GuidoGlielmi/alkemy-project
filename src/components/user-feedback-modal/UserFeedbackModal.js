5502fb1eddce4755829c2147e80f5bcda239d2ad
guidoglielmi <guidoglielmi@gmail.com>
2022-07-27 14:40:00 -0300

    fixed bug with toastify modal

--- src/components/user-feedback-modal/UserFeedbackModal.js
+++ src/components/user-feedback-modal/UserFeedbackModal.js
@@ -2,10 +2,11 @@ import {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {ToastContainer, toast} from 'react-toastify';
 import {clearUserFeedbackMsg} from 'redux/actions/tasksActions';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function UserFeedbackModal() {
   const dispatch = useDispatch();
-  const {userFeedbackMsg, error} = useSelector((state) => state);
+  const {userFeedbackMsg, error} = useSelector(state => state);
   useEffect(() => {
     const onClose = () => dispatch(clearUserFeedbackMsg());
     if (error) toast.error(userFeedbackMsg || 'Ha ocurrido un error', {onClose});
