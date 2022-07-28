d3e0be8bb9e62137370afeff00b3b7c06cf798f1
guidoglielmi <guidoglielmi@gmail.com>
2022-07-27 23:05:35 -0300

    improved filtering on Tasks component

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, Fragment} from 'react';
+import {useState, useEffect, Fragment, useMemo} from 'react';
 import Skeleton from 'react-loading-skeleton';
 import TaskGroup from 'components/task-group/TaskGroup';
 import {useSelector} from 'react-redux';
@@ -7,17 +7,17 @@ import styles from './Tasks.module.css';
 const device = window.innerWidth < 768;
 
 export default function Tasks({selectedPriority, searchKey}) {
-  const {tasks} = useSelector((state) => state);
+  const {tasks} = useSelector(state => state);
   const isFirstload = useIsFirstLoad(tasks.length);
 
-  function filterTasks() {
-    let filteredTasks = [...tasks];
+  const filteredTasks = useMemo(() => {
+    let ft = [...tasks];
     if (selectedPriority !== 'ALL') {
-      filteredTasks = filteredTasks.filter((t) => t.importance === selectedPriority);
+      ft = ft.filter(t => t.importance === selectedPriority);
     }
-    if (searchKey) filteredTasks = filteredTasks.filter((t) => t.title.includes(searchKey));
-    return filteredTasks;
-  }
+    if (searchKey) ft = ft.filter(t => t.title.toLowerCase().includes(searchKey.toLowerCase()));
+    return ft.length ? groupByStatus(ft) : [];
+  }, [tasks, selectedPriority, searchKey]);
 
   return (
     <div className={styles.tasks}>
@@ -25,11 +25,13 @@ export default function Tasks({selectedPriority, searchKey}) {
         .return(<Skeletons amount={3} />)
         .elseWhen(!tasks.length)
         .return(<span>No se han creado tareas</span>)
-        .else(
-          groupByStatus(filterTasks()).map(([status, tasks]) =>
-            tasks ? <TaskGroup key={status} status={status} tasks={tasks} /> : status,
-          ),
-        )}
+        .elseWhen(!!filteredTasks.length)
+        .return(
+          filteredTasks.map(([status, ts]) => (
+            <TaskGroup key={status} status={status} tasks={ts} />
+          )),
+        )
+        .else(<p style={{margin: '5vh auto 15vh'}}>No se han encontrado tareas</p>)}
     </div>
   );
 }
@@ -56,14 +58,13 @@ function useIsFirstLoad(condition = true) {
 }
 
 function groupByStatus(arr) {
-  if (!arr.length) return [[<p style={{margin: '5vh auto 15vh'}}>No se han encontrado tareas</p>]];
   const groupedTasks = arr.reduce((p, c) => ({...p, [c.status]: [...(p[c.status] || []), c]}), {});
   return Object.entries(groupedTasks).sort(([[status1]], [[status2]]) => status1 < status2);
 }
 
 export const when = (condition, value) => ({
-  elseWhen: (newCondition) => when(condition || newCondition, value),
-  return: (v) => when(condition, condition === true ? value ?? v : null),
-  else: (v) => value ?? v,
+  elseWhen: newCondition => when(condition || newCondition, value),
+  return: v => when(condition, condition === true ? value ?? v : null),
+  else: v => value ?? v,
   get: value,
 });
