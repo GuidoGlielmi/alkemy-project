8def934bb0c94f9dbf32a4f482881cc35859999f
guidoglielmi <guidoglielmi@gmail.com>
2022-08-01 14:04:02 -0300

    Fixed localStorage error in cache.ts

--- src/services/cache.js
+++ src/services/cache.js
@@ -1,13 +1,13 @@
 const hasToken = () => !!localStorage.getItem('token');
-const setToken = (token) => localStorage.setItem('token', token);
-const deleteToken = () => localStorage.deleteItem('token');
+const setToken = token => localStorage.setItem('token', token);
+const deleteToken = () => localStorage.removeItem('token');
 const getUsername = () => localStorage.getItem('username') || '';
-const setUsername = (username) => localStorage.setItem('username', username);
+const setUsername = username => localStorage.setItem('username', username);
 const isTeamLeader = () => !!localStorage.getItem('isTeamLeader');
-const setIsTeamLeader = (isTeamLeader) => localStorage.setItem('isTeamLeader', isTeamLeader);
-const deleteIsTeamLeader = () => localStorage.deleteItem('isTeamLeader');
+const setIsTeamLeader = isTeamLeader => localStorage.setItem('isTeamLeader', isTeamLeader);
+const deleteIsTeamLeader = () => localStorage.removeItem('isTeamLeader');
 const getTeamId = () => localStorage.getItem('teamID') || '';
-const setTeamId = (teamId) => localStorage.setItem('teamID', teamId);
+const setTeamId = teamId => localStorage.setItem('teamID', teamId);
 const clear = () => localStorage.clear();
 
 export default {
